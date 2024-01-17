d80807cf7e413105df5eb5d62b4c1924ed4cc15a
James <james.san@datascopeplc.com>
2024-01-17 16:32:41 +0000

    Amended Setup

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,58 +1,42 @@
 return {
   {
-    "loctvl842/monokai-pro.nvim",
+    "talha-akram/noctis.nvim",
     lazy = false,
-    priority = 1000,
-    keys = { { "<leader>um", "<cmd>MonokaiProSelect<cr>", desc = "Select Moonokai pro filter" } },
-    config = function()
-      local monokai = require("monokai-pro")
-      monokai.setup({
-        transparent_background = true,
-        devicons = true,
-        filter = "spectrum", -- classic | octagon | pro | machine | ristretto | spectrum
-        day_night = {
-          enable = false,
-          day_filter = "classic",
-          night_filter = "octagon",
-        },
-        inc_search = "background", -- underline | background
-        background_clear = {},
-        plugins = {
-          bufferline = {
-            underline_selected = true,
-            underline_visible = false,
-            bold = false,
-          },
-          indent_blankline = {
-            context_highlight = "pro", -- default | pro
-            context_start_underline = true,
-          },
-        },
-      })
-      monokai.load()
-    end,
+    pritory = 1000
   },
   {
     "nvimdev/dashboard-nvim",
     event = "VimEnter",
     opts = function(_, opts)
+
       local logo = [[
-██╗░░░██╗░██████╗░█████╗░░█████╗░██████╗░███████╗ 
-██║░░░██║██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔════╝
-╚██╗░██╔╝╚█████╗░██║░░╚═╝██║░░██║██║░░██║█████╗░░
-░╚████╔╝░░╚═══██╗██║░░██╗██║░░██║██║░░██║██╔══╝░░
-░░╚██╔╝░░██████╔╝╚█████╔╝╚█████╔╝██████╔╝███████╗
-░░░╚═╝░░░╚═════╝░░╚════╝░░╚════╝░╚═════╝░╚══════╝
-⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣶⣦⣀⡀
-⠀⣀⡀⠀⠀⠀⣠⣶⣿⣿⣿⢾⣽⣻
-⠘⢿⣿⣦⣠⣾⣿⣿⠟⠋⢸⣟⡾⣽
-⠀⠀⣹⣿⣿⣿⣏⠀⠀⠀⢹⣾⣹⣿
-⢠⣾⣿⠿⠻⢿⣿⣷⣤⣀⢸⣷⣻⢾
-⠀⢙⠅⠀⠀⠀⠙⢿⣿⣿⣟⡾⣽⣻
-⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⡯⠟⠃ 
+8888888 888b    888  .d8888b.  88888888888     d8888   .d8888b.  888    888        d8888  8888888b.  
+  888   8888b   888 d88P  Y88b     888        d88888  d88P  Y88b 888    888       d88888  888  "Y88b 
+  888   88888b  888 Y88b.          888       d88P888  888    888 888    888      d88P888  888    888 
+  888   888Y88b 888  "Y888b.       888      d88P 888  888        8888888888     d88P 888  888    888 
+  888   888 Y88b888     "Y88b.     888     d88P  888  888        888    888    d88P  888  888    888 
+  888   888  Y88888       "888     888    d88P   888  888    888 888    888   d88P   888  888    888 
+  888   888   Y8888 Y88b  d88P     888   d8888888888  Y88b  d88P 888    888  d8888888888  888  .d88P 
+8888888 888    Y888  "Y8888P"      888  d88P     888   "Y8888P"  888    888 d88P     888  8888888P"  
+                                                                                                      
+⣿⠄⡇⢸⣟⠄⠁⢸⡽⠖⠛⠈⡉⣉⠉⠋⣁⢘⠉⢉⠛⡿⢿⣿⣿⣿⣿⣿⣿⣿
+⣷⣶⣷⣤⠄⣠⠖⠁⠄⠂⠁⠄⠄⠉⠄⠄⠎⠄⠠⠎⢐⠄⢑⣛⠻⣿⣿⣿⣿⣿
+⣿⣿⣿⠓⠨⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠈⠐⠅⠄⠉⠄⠗⠆⣸⣿⣿⣿⣿⣿
+⣿⣿⣿⡣⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⢰⣤⣦⠄⠄⠄⠄⠄⠄⠄⡀⡙⣿⣿⣿⣿
+⣿⣿⡛⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠔⠿⡿⠿⠒⠄⠠⢤⡀⡀⠄⠁⠄⢻⣿⣿⣿
+⣿⣿⠄⠄⠄⠄⠄⠄⣠⡖⠄⠁⠁⠄⠄⠄⠄⠄⠄⠄⣽⠟⡖⠄⠄⠄⣼⣿⣿⣿
+⣿⣿⠄⠄⠄⠄⠄⠄⢠⣠⣀⠄⠄⠄⠄⢀⣾⣧⠄⠂⠸⣈⡏⠄⠄⠄⣿⣿⣿⣿
+⣿⣿⡞⠄⠄⠄⠄⠄⢸⣿⣶⣶⣶⣶⣶⡿⢻⡿⣻⣶⣿⣿⡇⠄⠄⠄⣿⣿⣿⣿
+⣿⣿⡷⡂⠄⠄⠁⠄⠸⣿⣿⣿⣿⣿⠟⠛⠉⠉⠙⠛⢿⣿⡇⠄⠄⢀⣿⣿⣿⣿
+⣶⣶⠃⠄⠄⠄⠄⠄⠄⣾⣿⣿⡿⠁⣀⣀⣤⣤⣤⣄⢈⣿⡇⠄⠄⢸⣿⣿⣿⣿
+⣿⣯⠄⠄⠄⠄⠄⠄⠄⢻⣿⣿⣷⣶⣿⣿⣥⣬⣿⣿⣟⣿⠃⠄⠨⠺⢿⣿⣿⣿
+⠱⠂⠄⠄⠄⠄⠄⠄⠄⣬⣸⡝⠿⢿⣿⡿⣿⠻⠟⠻⢫⡁⠄⠄⠄⡐⣾⣿⣿⣿
+⡜⠄⠄⠄⠄⠄⠆⡐⡇⢿⣽⣻⣷⣦⣧⡀⡀⠄⠄⣴⣺⡇⠄⠁⠄⢣⣿⣿⣿⣿
+⠡⠱⠄⠄⠡⠄⢠⣷⠆⢸⣿⣿⣿⣿⣿⣿⣷⣿⣾⣿⣿⡇⠄⠄⠠⠁⠿⣿⣿⣿
+⢀⣲⣧⣷⣿⢂⣄⡉⠄⠘⠿⣿⣿⣿⡟⣻⣯⠿⠟⠋⠉⢰⢦⠄⠊⢾⣷⣮⣽⣛
       ]]
 
-      logo = logo .. "\n"
+      logo = string.rep("\n", 8) .. logo .. "\n"
       opts.config.header = vim.split(logo, "\n")
     end,
   },
